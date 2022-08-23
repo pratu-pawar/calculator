07bb8295f51d051944fcdfd4e677b15366ba0a74
Spawar1530 <spawar1530@gmail.com>
2022-08-23 21:45:11 +0530

    minor mistakes in spellings and script tag was not added

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 
 let screen = document.getElementById('screen');
-buttons = document .querySelector('button');
+buttons = document.querySelectorAll('button');
 let screenValue = '';
-for(item of buttons){ 
-    item.addEventlistener('click', (e) =>{ 
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        console.log('button text is',buttonText);
-        if(buttonText== 'X'){ 
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
             buttonText = '*';
             screenValue += buttonText;
-            screen.value = screenValue
-            
-        }
-        else if (buttonText =='C'){ 
-            screenValue = "=";
             screen.value = screenValue;
         }
-        else if (buttonText== '='){ 
-            screen.value = evel(screenValue)
-
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
         }
-        else{ 
-            screenValue +=buttonText;
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
             screen.value = screenValue;
         }
 
